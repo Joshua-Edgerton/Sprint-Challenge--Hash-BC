843ea3fc8a22f19e032da8c7c9db80ffafcfd8f0
Joshua Edgerton <joshuaxedgerton@gmail.com>
2020-04-10 11:00:15 -0500

    hashtables ex1 tests passing, added more comments

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -10,13 +10,16 @@ def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
 
     # For each item in the range of the input length
+    # "i" will be used to loop through array
     for i in range(length):
         print("Step 1 - Each item within the length ->", i)
         # Use the insert function
         # Inserting into the hash table the input weights
+        # Insert function takes paramaters of hashtable/key/value
         hash_table_insert(ht, weights[i], i)
         # Index gets its value from the retrieve function
-        # Grabbing the weight limit minus the weight of each package
+        # Grabbing the weight limit minus the weight of each package for the key
+        # Retrieve function takes hashtable/key
         index = hash_table_retrieve(ht, (limit - weights[i]))
         print("Step 2 - Index value after retrieve function ->", index)
         # 2nd Index gets its value

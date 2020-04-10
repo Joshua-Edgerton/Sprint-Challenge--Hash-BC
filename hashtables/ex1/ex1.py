8d5092f4cd70b1f89d04b8c444a3617ebba4e628
Joshua Edgerton <joshuaxedgerton@gmail.com>
2020-04-10 10:54:17 -0500

    hashtables ex1 tests passing

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -9,11 +9,28 @@ from hashtables import (HashTable,
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
 
-    """
-    YOUR CODE HERE
-    """
-
-    return None
+    # For each item in the range of the input length
+    for i in range(length):
+        print("Step 1 - Each item within the length ->", i)
+        # Use the insert function
+        # Inserting into the hash table the input weights
+        hash_table_insert(ht, weights[i], i)
+        # Index gets its value from the retrieve function
+        # Grabbing the weight limit minus the weight of each package
+        index = hash_table_retrieve(ht, (limit - weights[i]))
+        print("Step 2 - Index value after retrieve function ->", index)
+        # 2nd Index gets its value
+        second_index = weights.index(weights[i])
+        print("Step 3 - Second_index value ->", second_index)
+        # If index exists and it is not equal to the 2nd index
+        if index is not None and index != second_index:
+            # Then if that 2nd index is greater than the first index
+            if second_index > index:
+                # Return the 2nd index first
+                return (second_index, index)
+            else:
+                # Otherwise returns the first index first
+                return (index, second_index)
 
 
 def print_answer(answer):
@@ -21,3 +38,5 @@ def print_answer(answer):
         print(str(answer[0] + " " + answer[1]))
     else:
         print("None")
+
+print(get_indices_of_item_weights([4, 4], 2, 8))
